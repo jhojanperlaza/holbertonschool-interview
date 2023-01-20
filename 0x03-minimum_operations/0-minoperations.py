e03220efad7bb38565422251d9cf67a8ff99e1e5
jhojan <jhojan_perlaza123>
2023-01-20 17:05:38 -0500

    [ADD] task

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 Minimum Operations
+video guia: https://www.youtube.com/watch?v=03ZepmoVJGI&ab_channel=TECHDOSE
 """
 
-
+"""V1
 def minOperations(n):
     if n <= 1:
         return (0)
@@ -16,5 +17,21 @@ def minOperations(n):
         else:
             num_op += 1
             n -= 1
+
+    return(num_op)
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return (0)
+
+    num_op = 0
+    cont_op = 2
+    while n > 1:
+        if n % cont_op == 0:
+            num_op += cont_op
+            n /= cont_op
+        else:
+            cont_op += 1
 
     return(num_op)
