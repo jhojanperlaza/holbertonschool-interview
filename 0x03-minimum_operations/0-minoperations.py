3b97431e999794358a559ff12c69d1c61ccacb9b
jhojan <jhojan_perlaza123>
2023-01-20 14:31:20 -0500

    [ADD] task

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,12 +6,16 @@ def minOperations(n):
     if n <= 0:
         return (0)
     num_H = 1
+    num_op = 1
     while num_H != n:
         if num_H * 2 == n:
-            num_H += 2
+            num_H *= 2
+            num_op += 2
         elif num_H * 3 == n:
-            num_H += 3
+            num_H *= 3
+            num_op += 3
         else:
             num_H += 1
+            num_op += 1
 
-    return(num_H)
+    return(num_op)
